a9328eccf855075fff5d1f6757e1910da88ec306
Ankit Agarwal <ankitagarwal@Ankits-MacBook-Pro.local>
2024-02-23 18:04:04 +0530

    feat: workflow updates

--- src/encryption/lit/tests/access.test.ts
+++ src/encryption/lit/tests/access.test.ts
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// FILEPATH: /Users/ankitagarwal/Documents/projects/np-sdk/src/encryption/lit/tests/access.test.ts
 
 import { getDefaultAuth, ConditionType, getContentAuth } from '../access'
 
